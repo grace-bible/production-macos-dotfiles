6ab7bd66a65987c54858a4aa430dd251c57c6fd8
Elliot Matson <1711604+elliotmatson@users.noreply.github.com>
2023-07-24 14:22:39 -0500

    Update macos.sh
    Remove Activity monitor settings

--- macos.sh
+++ macos.sh
@@ -90,23 +90,6 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 ###############################################################################
-# Activity Monitor                                                            #
-###############################################################################
-
-# Show the main window when launching Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
-# Visualize CPU usage in the Activity Monitor Dock icon
-defaults write com.apple.ActivityMonitor IconType -int 5
-
-# Show all processes in Activity Monitor
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
-
-# Sort Activity Monitor results by CPU usage
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
